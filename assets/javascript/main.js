f9200c81355d54bb866e03f3aec32ef92fb0d472
Ryan BrodeFrank <ryanafrank@gmail.com>
2018-11-24 22:24:57 -0600

    add html formating with 3 rows: 1 jumbotron, 1 train schedule table, 1 new train submission form

---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbK_0U7e_gmTiC92HLlWleJOSUUKKOAs8",
+    authDomain: "train-scheduler-573d7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-573d7.firebaseio.com",
+    projectId: "train-scheduler-573d7",
+    storageBucket: "train-scheduler-573d7.appspot.com",
+    messagingSenderId: "2202145889"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
